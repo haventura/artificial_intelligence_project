799a403930b7e2361f009ace727d0f1b01e1d81e
haventura <a.ventura@students.ephec.be>
2022-11-16 12:05:30 +0100

    added files post request in server

--- backend/README.md
+++ backend/README.md
@@ -7,3 +7,5 @@ digit recog https://nextjournal.com/gkoehler/digit-recognition-with-keras
 https://fastapi.tiangolo.com/
 
 files with fastapi: https://fastapi.tiangolo.com/tutorial/request-files/?h=files
+
+run server with python -m uvicorn main:app --reload
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -13,3 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
