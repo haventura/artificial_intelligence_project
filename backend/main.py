24ac9f68c71731c10ec222a998a61032e3f77722
haventura <a.ventura@students.ephec.be>
2022-12-07 12:00:43 +0100

    moved model location and added word extraction in main

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,9 @@ import time
 import json
 from SimpleHTR.src.inference import infer
 from SimpleHTR.src.model import Model
+from WordDetectorNN.src.infer import word_extractor
 import dataclasses
+import os
 
 class FilePaths:
     """Filenames and paths to data."""
@@ -54,14 +56,47 @@ def read_image(image_id: int):
 @app.post("/uploadfile/")
 async def create_file(file: bytes = File()):
     timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    #create subfolder in data/ with timestamp as name
+    #this folder will contain the main uploaded image file and all subwords image files
+    dirname = os.path.dirname(__file__)
+    main_image_folder_path = os.path.join(dirname, f'data/{timestamp}')
+    sub_images_folder_path = os.path.join(dirname, f'data/{timestamp}/sub_images')
+    os.mkdir(main_image_folder_path)
+    os.mkdir(sub_images_folder_path)
+
     filename = f'{timestamp}.png'
     f = open(f'data/{filename}', 'wb')
     f.write(file)
     f.close()
 
-    recognized, probability = infer(model, f'data/{filename}')
-    text_data = [TextData("#ff00ff",recognized), TextData("#ff00ff",recognized)]
+    # Call Dawid's word detector function, passing the main image file
+    word_count = word_extractor(f"data/{timestamp}", f"data/{timestamp}/sub_images")
+    # Dawid will make sure that each subimage is labelled orderly
+    # Then loop over each sub_image, transcribe them one by one and append each word to a list.
+
+    # list[dawidReturnedColor], list[paragraphFolderName]
+    # or a list of dictionnaries?
+    # a 2-D array of colors and associated folder (will only contain 1 entry for a start)
+    # dico = {colors: [color 1, color 2, color 3], folders: [paragraphfolder1, paragraphfolder2, paragraphfolder3]}
+
+    # text_data = []
+    # For i in len(dico[folders]):
+    # 
+    #   color = dico[colors][i]
+    #   For each image in dico[folders][i]:
+    #   recognized, probability = infer(model, f'data/{timestamp}/{subfolder}/{image}')
+    #   data = TextData(color, recognized)
+    #   text_data.append(data)
+    #   
+    # response_data = ResponseData("nom de l'image", "une url", text_data)
+    text = ""
+    for filename in os.listdir(sub_images_folder_path):
+        recognized, probability = infer(model, f'{sub_images_folder_path}/{filename}')
+        text += (recognized + " ")
+
+    text_data = [TextData("#ff00ff",text), TextData("#ff00ff",text)]
     response_data = ResponseData("nom de l'image", "une url", text_data)
-    with open(FilePaths.fn_output, 'w') as f:
-        json.dump(dataclasses.asdict(response_data),f)
+    #with open(FilePaths.fn_output, 'w') as f:
+    #    json.dump(dataclasses.asdict(response_data),f)
     return response_data
