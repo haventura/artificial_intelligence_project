348522a367469708833d6048f81d21b808f3bbc4
haventura <a.ventura@students.ephec.be>
2023-01-01 15:56:27 +0100

    fixed colors issue, works well if requests arent mitrailled

--- frontend/main.py
+++ frontend/main.py
@@ -5,11 +5,26 @@ import pandas as pd
 import numpy as np
 import requests
 import io
-import json
+
+class fragile(object):
+    class Break(Exception):
+      """Break out of the with statement"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __enter__(self):
+        return self.value.__enter__()
+
+    def __exit__(self, etype, value, traceback):
+        error = self.value.__exit__(etype, value, traceback)
+        if etype == self.Break:
+            return True
+        return error
 
 def main():
-    st.set_page_config(page_title="Text Recognition", page_icon="🚀", layout="wide")
-    st.title("Handwritten Text Recognition")
+    st.set_page_config(page_title="Text Recognition", page_icon="✏️", layout="wide")
+    st.title("✒️ Handwritten Text Recognition")
 
     col1, col2, col3 = st.columns([1,2,1])
     # image_placeholder = col2.empty()
@@ -24,56 +39,62 @@ def main():
 
     if "color_index" not in st.session_state:
         st.session_state["color_index"] = 0
+    print(st.session_state["color_index"])
     if "transcript" not in st.session_state:
         st.session_state["transcript"] = []
-    uploaded_image = col1.file_uploader("Text file:", type=["png", "jpg", "pdf"], accept_multiple_files=False)
-    with col2:
-        if(uploaded_image):
-            background_image = Image.open(uploaded_image) 
-            width = 500
-            height = 500 
-            if background_image.width >= background_image.height:
-                ratio = width / background_image.width
-                height = int(background_image.height * ratio)
-            else:
-                ratio = height / background_image.height
-                width = int(background_image.width * ratio)
-            canvas_result = st_canvas(
-                fill_color = "rgba(0, 0, 0, 0.0)",
-                stroke_color = COLORS[st.session_state["color_index"]],
-                stroke_width = 2,
-                background_image = background_image,
-                width = width,
-                height = height,
-                drawing_mode = "rect",
-                key = "canvas",
-                display_toolbar = False,
-            )
+
+    with col1:
+        st.header("📜 Input File")
+        uploaded_file = st.file_uploader("Drop a file containing handwritten text here:",type=["png", "jpg", "pdf"], accept_multiple_files=False)
+    
+    with fragile(col2):
+        if uploaded_file is None:
+            raise fragile.Break
+        background_image = Image.open(uploaded_file) 
+        width, height, ratio = scaleImage(background_image,500)
+        canvas_result = st_canvas(
+            fill_color = "rgba(0, 0, 0, 0.0)",
+            stroke_color = COLORS[st.session_state["color_index"]-1],
+            stroke_width = 2,
+            background_image = background_image,
+            width = width,
+            height = height,
+            drawing_mode = "rect",
+            key = "canvas",
+            display_toolbar = False,
+        )
+
+    with fragile(col3):
+        st.header("🖨️ Transcripts")
+        if uploaded_file is None:
+            raise fragile.Break
+        if canvas_result.json_data is None:
             
-    with col3:
-        st.header("Transcripts:")
-        if uploaded_image is not None and canvas_result.json_data is not None:
-            df = pd.json_normalize(canvas_result.json_data["objects"])
-            if len(df) > 0:
-                crop_result = canvas_result.json_data["objects"][-1]
-                left = crop_result["left"] * (1/ratio)
-                top = crop_result["top"] * (1/ratio)
-                right = left + crop_result["width"] * (1/ratio)
-                bottom = top + crop_result["height"] * (1/ratio)             
-                cropped_image = background_image.crop((left, top, right, bottom))
-                img_byte_arr = io.BytesIO()
-                cropped_image.save(img_byte_arr, format='PNG')
-                img_byte_arr = img_byte_arr.getvalue()
-                response = requests.post('http://localhost:8000/uploadfile/', files = {'file': img_byte_arr})
-                transcript = response.json()["content"]
-                st.session_state["transcript"].append((COLORS[st.session_state["color_index"]], transcript))
-                st.session_state["color_index"] += 1
-                if(st.session_state["color_index"] >= len(COLORS)):
-                    st.session_state["color_index"] = 0
-                for value in st.session_state["transcript"]:
-                    st.write('<p style="color:' + value[0] + ';">' + value[1] + '</p>', unsafe_allow_html=True)
+            raise fragile.Break
+        df = pd.json_normalize(canvas_result.json_data["objects"])
+       
+        if len(df) == 0:
+            st.session_state["color_index"] += 1
+            raise fragile.Break
+        crop_result = canvas_result.json_data["objects"][-1]
+        left = crop_result["left"] * (1/ratio)
+        top = crop_result["top"] * (1/ratio)
+        right = left + crop_result["width"] * (1/ratio)
+        bottom = top + crop_result["height"] * (1/ratio)             
+        cropped_image = background_image.crop((left, top, right, bottom))
+        img_byte_arr = io.BytesIO()
+        cropped_image.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()     
+        response = requests.post('http://localhost:8000/uploadfile/', files = {'file': img_byte_arr})
+        transcript = response.json()["content"]
+        st.session_state["transcript"].append((COLORS[st.session_state["color_index"]-2], transcript))
+        for value in st.session_state["transcript"]:
+            st.write('<p style="color:' + value[0] + ';">' + value[1] + '</p>', unsafe_allow_html=True)
+        st.session_state["color_index"] += 1
+        if(st.session_state["color_index"] >= len(COLORS)):
+            st.session_state["color_index"] = 0
         
-    st.write('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+    st.write('<style>div.block-container{padding-top:1.2rem;}</style>', unsafe_allow_html=True)
     hide_footer_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -82,6 +103,18 @@ def main():
             """
     st.markdown(hide_footer_style, unsafe_allow_html=True) 
     st.caption("Made with Tensorflow and Streamlit by Andrea Ventura, Dawid Krasowski, Bartlomiej Drewnowski. <a href=https://github.com/haventura/artificial_intelligence_project>GitHub project</a>", unsafe_allow_html=True)
+
+def scaleImage(image, size):
+    if image.width >= image.height:
+        width = size
+        ratio = width / image.width
+        height = int(image.height * ratio)
+        return width, height, ratio
+    else:
+        height = size
+        ratio = height / image.height
+        width = int(image.width * ratio)
+        return width, height, ratio
 
 if __name__ == '__main__':
     main()
